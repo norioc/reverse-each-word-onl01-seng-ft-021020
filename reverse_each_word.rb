81be739a5428a5f1358ee23806e2e66e3f9af512
norio c <norionorioc@gmail.com>
2020-02-13 02:18:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(sentence)
+  
+  old_sentence = sentence.split("")
   new_sentence = []
-  old_sentence = sentence.split
   old_sentence.each do |word| 
   new_sentence << word.reverse 
   
 end
-  new_sentence.join(" ,")
+  new_sentence.join("")
   
 end
 

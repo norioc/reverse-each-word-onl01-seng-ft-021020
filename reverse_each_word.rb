c2e877d1e0a2c4f9b23bd6c27636b6053db946c1
norio c <norionorioc@gmail.com>
2020-02-13 02:22:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,8 @@
-def reverse_each_word(sentence)
-  
-  old_sentence = sentence.split("")
-  new_sentence = [ ]
-  old_sentence.each do |word| 
-  new_sentence << word.reverse 
-  
+
+
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
-  return new_sentence.join("")
-  
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-def reverse_each_word(sentence)
-  old_sentence = sentence.split
-  old_sentence.collect do |word| 
-  word.reverse 
-end
-  
-end
-
-

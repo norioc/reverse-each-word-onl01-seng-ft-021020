85d96b92c42914e539557c848e2af2b14d904a94
norio c <norionorioc@gmail.com>
2020-02-13 02:24:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,11 @@ def reverse_each_word(array)
   return reversed_array.join(" ")
 end
 
-
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.collect {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
 
 
 
